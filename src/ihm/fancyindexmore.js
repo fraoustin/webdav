5bf360514fb364191ed1b1e95b641ecd5d7c9755
fraoustin <fraoustin@gmail.com>
2018-06-24 19:48:52 +0200

    webdav.js

---
+++ src/ihm/fancyindexmore.js
@@ -0,0 +1,9 @@
+function clickGetInfo(id) {
+    console.log("coucou")
+}
+
+// test webdav.js
+//var fs = new WebDAV.Fs('http://127.0.0.1');
+//var hello = fs.file('/nicole/DSC06197.JPG');
+//hello.rm();
+//console.log("finish")
